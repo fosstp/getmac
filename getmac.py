a7f85ba454de16bbb7357287e8aa01d14fe0fb5e
Alex Wang <yen867@gmail.com>
2016-09-10 15:36:42 +0800

    PEP8

--- getmac.py
+++ getmac.py
@@ -11,41 +11,49 @@ db_pwd = ''
 hostname = os.environ['COMPUTERNAME']
 mac_addr = hex(uuid.getnode()).replace('0x', '').upper()
 try:
-    conn = sql.connect(host = db_host, database = db_name, user = db_user, passwd = db_pwd)
+    conn = sql.connect(host=db_host, database=db_name,
+                       user=db_user, passwd=db_pwd)
 except:
-	#print ("資料庫連結失敗")
-	input("資料庫連結失敗 請按 Enter 結束...")
-	exit()
+    # print ("資料庫連結失敗")
+    input("資料庫連結失敗 請按 Enter 結束...")
+    exit()
 
 # insert
+
+
 def ins(hostname, mac_addr):
     ins_data = (hostname, mac_addr)
     c = conn.cursor()
-    q = ("INSERT INTO net_data (name, mac) VALUES (%s, %s)") 
+    q = ("INSERT INTO net_data (name, mac) VALUES (%s, %s)")
     a = c.execute(q, ins_data)
     conn.commit()
     c.close()
 
 # update
+
+
 def upd(hostname, mac_addr):
     upd_data = (hostname, mac_addr)
     c = conn.cursor()
-    q = ("UPDATE net_data SET name = %s WHERE mac = %s") 
+    q = ("UPDATE net_data SET name = %s WHERE mac = %s")
     a = c.execute(q, upd_data)
     conn.commit()
     c.close()
 
 # check duplicate
+
+
 def is_duplicate(mac_addr):
     c = conn.cursor()
     q = ("SELECT id FROM net_data WHERE mac = %s")
     a = c.execute(q, (mac_addr,))
     r = c.fetchone()
-    if r :
+    if r:
         return True
     else:
-        return False 
-        
+        return False
+
+
 def main():
     if is_duplicate(mac_addr):
         upd(hostname, mac_addr)
@@ -54,4 +62,5 @@ def main():
     pass
 
 if __name__ == "__main__":
-    	main()
+        main()
+
